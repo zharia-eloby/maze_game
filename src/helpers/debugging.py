0a37ef3197cd237ba3bb2428e2b51791493c1789
zharia-eloby <55672825+zharia-eloby@users.noreply.github.com>
2024-07-21 14:32:30 -0500

    updated file path definitions

--- src/helpers/debugging.py
+++ src/helpers/debugging.py
@@ -15,18 +15,18 @@ def print_maze(maze):
 def resize_image(image_id, width, height, normal=True, hovered=True):
     if resize_images:
         settings = get_settings()
-        theme_file = os.path.join(sys.path[0], settings['theme']['path'])
+        theme_file = os.path.join(sys.path[0], os.path.relpath(settings['theme']['path'], sys.path[0]))
         file = open(theme_file, "r")
         contents = json.loads(file.read())
         file.close()
-        images_folder = os.path.join(sys.path[0], "assets/images/")
+        images_folder = os.path.join(sys.path[0], os.path.relpath("src/assets/images/", sys.path[0]))
         if (normal):
             image_file = contents[image_id]['images']['normal_image']['resource']
-            img = Image.open(images_folder + image_file)
+            img = Image.open(os.path.join(images_folder, image_file))
             img = img.resize((width, height))
-            img = img.save(images_folder + image_file)
+            img = img.save(os.path.join(images_folder, image_file))
         if (hovered):
             image_file = contents[image_id]['images']['hovered_image']['resource']
-            img = Image.open(images_folder + image_file)
+            img = Image.open(os.path.join(images_folder, image_file))
             img = img.resize((width, height))
-            img.save(images_folder + image_file)
+            img.save(os.path.join(images_folder, image_file))
