b75eba059f12d67723e42bf535e8d2fa48c0e600
zharia-eloby <55672825+zharia-eloby@users.noreply.github.com>
2024-08-16 13:03:02 -0500

    updated credits screen

--- src/classes/screens/credits_screen.py
+++ src/classes/screens/credits_screen.py
@@ -1,4 +1,4 @@
-import pygame, pygame_gui, math, time
+import pygame, pygame_gui, math, time, webbrowser
 from pygame_gui.core import ObjectID
 from classes.screens.screen import Screen
 
@@ -8,6 +8,25 @@ class CreditsScreen(Screen):
         self.audio = audio
         self.line_spacing = 15
         self.managers = [self.get_background()['background_manager'], self.ui_manager]
+        self.credits_column_width = self.game_window.drawable_area.width*0.75
+        self.links_column_width = self.game_window.drawable_area.width - self.credits_column_width
+        self.credits = {
+            "#audio-credits": {
+                "text": "Audio from",
+                "link": "https://space-spheremaps.itch.io/pixelart-starfields",
+                "object_id": "#audio-credits"
+            },
+            "#background-credits": {
+                "text": "Background from Space Spheremaps",
+                "link": "https://space-spheremaps.itch.io/pixelart-starfields",
+                "object_id": "#background-credits"
+            },
+            "#icons-credits": {
+                "text": "Icons from Kenney.nl",
+                "link": "https://kenney.nl/assets/game-icons",
+                "object_id": "#icons-credits"
+            }
+        }
 
     def setup(self):
         self.audio.create_audio_buttons(self.ui_manager)
@@ -27,9 +46,9 @@ class CreditsScreen(Screen):
 
         game_title_text_rect = pygame.Rect(
             self.game_window.drawable_area.left, 
-            self.game_window.drawable_area.top,
+            back_button_rect.bottom,
             self.game_window.drawable_area.width, 
-            back_button_rect.bottom + self.line_spacing
+            self.game_window.medium_text_height
         )
         pygame_gui.elements.UILabel(
             relative_rect=game_title_text_rect, 
@@ -40,9 +59,9 @@ class CreditsScreen(Screen):
         
         development_credits_text = pygame.Rect(
             self.game_window.drawable_area.left, 
-            self.game_window.drawable_area.top,
-            self.game_window.drawable_area.width, 
-            game_title_text_rect.bottom + self.line_spacing
+            game_title_text_rect.bottom + self.line_spacing,
+            self.game_window.drawable_area.width,
+            self.game_window.small_text_height
         )
         pygame_gui.elements.UILabel(
             relative_rect=development_credits_text, 
@@ -50,6 +69,34 @@ class CreditsScreen(Screen):
             manager=self.ui_manager,
             object_id=ObjectID(object_id="@small-text")
         )
+
+        previous_rect = development_credits_text
+        for i in self.credits:
+            label_rect = pygame.Rect(
+                self.game_window.drawable_area.left,
+                previous_rect.bottom + self.line_spacing,
+                self.credits_column_width,
+                self.game_window.small_text_height
+            )
+            pygame_gui.elements.UILabel(
+                relative_rect=label_rect,
+                text=self.credits[i]["text"],
+                manager=self.ui_manager,
+                object_id=ObjectID(class_id="@small-text")
+            )
+            button_rect = pygame.Rect(
+                label_rect.right,
+                previous_rect.bottom + self.line_spacing,
+                self.links_column_width,
+                self.game_window.small_text_height
+            )
+            pygame_gui.elements.UIButton(
+                relative_rect=button_rect,
+                text="show me",
+                manager=self.ui_manager,
+                object_id=ObjectID(class_id="@small-text", object_id=self.credits[i]["object_id"])
+            )
+            previous_rect = label_rect
 
     def show(self):
         self.audio.set_audio_display()
@@ -68,6 +115,9 @@ class CreditsScreen(Screen):
                         done = True
                         next_page = self.game_window.settings_screen
                         break
+
+                    else:
+                        webbrowser.open(self.credits[event.ui_object_id]['link'])
 
                 elif event.type == pygame.WINDOWRESTORED:
                     pygame.display.update()
