934f73f858bbd683a59ca010d270413ee905576a
zharia-eloby <55672825+zharia-eloby@users.noreply.github.com>
2024-08-19 21:06:51 -0500

    updated run file
    added functions

--- src/run.py
+++ src/run.py
@@ -8,38 +8,42 @@ from classes.game_window import GameWindow
 from classes.screens.loading_screen import LoadingScreen
 from classes.audio import Audio
 
-gw = GameWindow()
-gw.initialize()
-
-def load_content():
-    audio = Audio(gw)
+def load_content(game_window):
+    audio = Audio(game_window)
     audio.initialize()
-    gw.initialize_screens(audio, gw)
-    gw.finished_loading = True
+    game_window.initialize_screens(audio, game_window)
+    game_window.finished_loading = True
 
-def show_loading_screen():
-    loading_screen = LoadingScreen(gw)
+def show_loading_screen(game_window):
+    loading_screen = LoadingScreen(game_window)
     loading_screen.setup()
     loading_screen.show()
 
-load_content_thread = threading.Thread(target=load_content)
-loading_screen_thread = threading.Thread(target=show_loading_screen)
-load_content_thread.start()
-loading_screen_thread.start()
-load_content_thread.join()
-loading_screen_thread.join()
+def run():
+    gw = GameWindow()
+    gw.initialize()
 
-pygame.event.clear()
+    load_content_thread = threading.Thread(target=load_content, args=(gw,))
+    loading_screen_thread = threading.Thread(target=show_loading_screen, args=(gw,))
+    load_content_thread.start()
+    loading_screen_thread.start()
+    load_content_thread.join()
+    loading_screen_thread.join()
 
-next_page = gw.title_screen
-done = False
-while not done:
-    next_page = next_page.show()
-    if not next_page:
-        done = True
+    pygame.event.clear()
 
-gw.settings['audio']['volume'] = pygame.mixer.music.get_volume()
-gw.settings['audio']['on'] = pygame.mixer.music.get_busy()
-gw.save_settings()
-pygame.quit()
-sys.exit()
+    next_page = gw.title_screen
+    done = False
+    while not done:
+        next_page = next_page.show()
+        if not next_page:
+            done = True
+
+    gw.settings['audio']['volume'] = pygame.mixer.music.get_volume()
+    gw.settings['audio']['on'] = pygame.mixer.music.get_busy()
+    gw.save_settings()
+
+    pygame.quit()
+    sys.exit()
+
+run()
