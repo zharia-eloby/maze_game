02a05999f12247e95e2171c66003e00bee8911f0
zharia-eloby <55672825+zharia-eloby@users.noreply.github.com>
2024-09-28 15:27:02 -0500

    updated loading screen

--- src/screens/loading_screen.py
+++ src/screens/loading_screen.py
@@ -1,13 +1,21 @@
-import pygame, pygame_gui
+import pygame, pygame_gui, os
 from pygame_gui.core import ObjectID
 from screens.screen import Screen
 
 class LoadingScreen(Screen):
     def __init__(self, game_window):
         super().__init__(game_window)
+        theme_file = os.path.realpath("src/assets/themes/loading/theme.json")
+        self.ui_manager = pygame_gui.UIManager((self.game_window.screen_width, self.game_window.screen_height), theme_file)
         self.managers = [self.ui_manager]
 
     def setup(self):
+        background_rect = pygame.Rect(0, 0, self.game_window.screen_width, self.game_window.screen_height)
+        pygame_gui.elements.UIPanel(
+            relative_rect=background_rect,
+            manager=self.ui_manager
+        )
+
         loading_text_rect = self.game_window.drawable_area
         pygame_gui.elements.UILabel(
             relative_rect=loading_text_rect, 
