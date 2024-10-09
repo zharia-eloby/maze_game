afb444ef10c447dd7f4324505e93d47eeda484aa
zharia-eloby <55672825+zharia-eloby@users.noreply.github.com>
2024-10-08 21:49:37 -0500

    added animation to loading screen

--- src/screens/loading_screen.py
+++ src/screens/loading_screen.py
@@ -1,4 +1,4 @@
-import pygame, pygame_gui, os
+import pygame, pygame_gui, os, math, time
 from pygame_gui.core import ObjectID
 from screens.screen import Screen
 
@@ -10,26 +10,45 @@ class LoadingScreen(Screen):
         self.managers = [self.ui_manager]
 
     def setup(self):
-        background_rect = pygame.Rect(0, 0, self.settings.screen_width, self.settings.screen_height)
+        self.background_rect = pygame.Rect(0, 0, self.settings.screen_width, self.settings.screen_height)
         pygame_gui.elements.UIPanel(
-            relative_rect=background_rect,
+            relative_rect=self.background_rect,
             manager=self.ui_manager
         )
 
         loading_text_rect = self.settings.drawable_area
         pygame_gui.elements.UILabel(
             relative_rect=loading_text_rect, 
-            text="Loading...",
+            text="Loading",
             manager=self.ui_manager,
             object_id=ObjectID(object_id="@medium-text")
         )
 
+        self.loading_line_max_length = round(self.background_rect.width/3)
+        self.loading_line_min_length = 2
+        loading_line_rect = pygame.Rect(
+            self.background_rect.centerx - round(self.loading_line_min_length/2),
+            self.background_rect.centery + round(self.settings.medium_text_height / 2),
+            self.loading_line_min_length,
+            10
+        )
+        self.loading_line = pygame_gui.elements.UIPanel(
+            relative_rect=loading_line_rect,
+            manager=self.ui_manager,
+            object_id=ObjectID(object_id="@line")
+        )
+
     def show(self):
         POLL = pygame.USEREVENT + 1
+        ANIMATE = pygame.USEREVENT + 2
+        
+        line_increment = 2
+        time_delta = math.ceil(time.time())
+
         pygame.time.set_timer(POLL, 1000)
+        pygame.time.set_timer(ANIMATE, 30)
         self.redraw_elements(self.managers, 0)
 
-        clock = pygame.time.Clock()
         done = False
         while not done:
             for event in [pygame.event.wait()]+pygame.event.get():
@@ -41,12 +60,26 @@ class LoadingScreen(Screen):
                     if self.game_window.finished_loading:
                         done = True
                         pygame.time.set_timer(POLL, 0)
+                        pygame.time.set_timer(ANIMATE, 0)
                         return
+                    
+                elif event.type == ANIMATE:
+                    current_width = self.loading_line.get_relative_rect().width
+                    if current_width + line_increment < self.loading_line_min_length or current_width + line_increment > self.loading_line_max_length:
+                        line_increment *= -1
+                    self.loading_line.set_dimensions((
+                        self.loading_line.get_relative_rect().width + line_increment, 
+                        self.loading_line.get_relative_rect().height
+                    ))
+                    self.loading_line.set_position((
+                        self.background_rect.centerx - self.loading_line.get_relative_rect().width/2, 
+                        self.loading_line.get_relative_rect().top
+                    ))
 
                 elif event.type == pygame.WINDOWRESTORED:
                     pygame.display.update()
 
                 self.ui_manager.process_events(event)
 
-            clock.tick(1)
-            self.redraw_elements(self.managers, 1)
+            time_delta = math.ceil(time.time()) - time_delta
+            self.redraw_elements(self.managers, time_delta)
