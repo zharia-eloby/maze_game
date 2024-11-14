8505bc4c5c21558f30c180dd34ebbd2f04870dd8
zharia-eloby <55672825+zharia-eloby@users.noreply.github.com>
2024-11-14 13:15:41 -0600

    added error handler & error screen

---
+++ src/general/error_handler.py
@@ -0,0 +1,22 @@
+import os, traceback
+
+class ErrorHandler:
+    def handle_error(error, debug_mode, error_screen):
+        if debug_mode: print(traceback.format_exc())
+
+        # write the error to the erorr log file
+        folder_path = os.path.realpath("./src/logs")
+        file_path = os.path.join(folder_path, "error_log.txt")
+
+        if not os.path.exists(folder_path): 
+            os.mkdir(folder_path)
+
+        with open(file_path, 'w') as f: 
+            traceback.print_exc(file=f)
+
+        # set error text for error screen
+        if (type(error) == FileNotFoundError):
+            filename = error.__str__().rsplit('\\')[-1].rstrip("\'\.")
+            error_screen.set_error_text('Missing file: {filename}'.format(filename=filename))
+        else:
+            error_screen.set_error_text(error.__class__.__name__)
---
+++ src/screens/error_screen.py
@@ -0,0 +1,112 @@
+import pygame, pygame_gui, os, math, time, webbrowser
+from screens.screen import Screen
+from pygame_gui.core import ObjectID
+
+class ErrorScreen(Screen):
+    def __init__(self, game_window):
+        super().__init__(game_window, None)
+        theme_file = os.path.realpath("src/themes/error/theme.json")
+        self.ui_manager = pygame_gui.UIManager((self.settings.screen_width, self.settings.screen_height), theme_file)
+        self.background_manager = pygame_gui.UIManager((self.settings.screen_width, self.settings.screen_height), theme_file)
+        self.managers = [self.background_manager, self.ui_manager]
+
+        self.error_text_box = None
+
+    def setup(self):
+        self.background_rect = pygame.Rect(0, 0, self.settings.screen_width, self.settings.screen_height)
+        pygame_gui.elements.UIPanel(
+            relative_rect=self.background_rect,
+            manager=self.background_manager
+        )
+
+        button_height = math.floor(self.settings.drawable_area.height/10)
+        exit_button_rect = pygame.Rect(
+            self.settings.drawable_area.left,
+            self.settings.drawable_area.bottom - button_height,
+            self.settings.drawable_area.width,
+            button_height
+        )
+        pygame_gui.elements.UIButton(
+            relative_rect=exit_button_rect, 
+            text="Close Window",
+            manager=self.ui_manager,
+            object_id=ObjectID("#exit-button", class_id="@small-text")
+        )
+
+        report_button_rect = pygame.Rect(
+            exit_button_rect.left,
+            exit_button_rect.top - button_height - self.settings.line_spacing,
+            exit_button_rect.width,
+            button_height
+        )
+        self.report_button = pygame_gui.elements.UIButton(
+            relative_rect=report_button_rect, 
+            text="Report An Issue",
+            manager=self.ui_manager,
+            object_id=ObjectID(object_id="#report-button", class_id="@small-text")
+        )
+
+        error_heading_rect = pygame.Rect(
+            self.settings.drawable_area.left,
+            self.settings.drawable_area.top,
+            self.settings.drawable_area.width,
+            (self.settings.drawable_area.height - button_height*2 - self.settings.line_spacing)/2
+        )
+        self.error_text_box = pygame_gui.elements.UITextBox(
+            relative_rect=error_heading_rect, 
+            html_text="<p>Uh oh! An error occurred.</p>",
+            manager=self.ui_manager,
+            object_id=ObjectID(object_id="#error-heading", class_id="@large-text")
+        )
+        error_text_rect = pygame.Rect(
+            error_heading_rect.left,
+            error_heading_rect.bottom,
+            self.settings.drawable_area.width,
+            report_button_rect.top-error_heading_rect.bottom
+        )
+        self.error_text_box = pygame_gui.elements.UITextBox(
+            relative_rect=error_text_rect, 
+            html_text="",
+            manager=self.ui_manager,
+            object_id=ObjectID(object_id="#error-text", class_id="@small-text")
+        )
+
+    def set_error_text(self, error_text):
+        if (os.name == "nt"): # for windows devices
+            help_text = "<a href=''>See full error log</a>"
+        else:
+            help_text = "See error log at '{file_path}'".format(file_path=os.path.realpath("./src/logs/error_log.txt"))
+        html_text = "<p>Error > {error_text}</p><p>{help_text}</p>".format(error_text=error_text, help_text=help_text)
+        self.error_text_box.set_text(html_text)
+
+    def show(self):
+        pygame.mixer.music.stop()
+        
+        time_delta = math.ceil(time.time())
+
+        self.redraw_elements(self.managers, 0)
+        pygame.event.clear()
+
+        done = False
+        while not done:
+            for event in [pygame.event.wait()]+pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+                    return
+
+                elif event.type == pygame.WINDOWRESTORED:
+                    pygame.display.update()
+
+                elif event.type == pygame_gui.UI_TEXT_BOX_LINK_CLICKED:
+                    os.startfile(os.path.realpath("./src/logs/error_log.txt"))
+                
+                elif event.type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_object_id == "#report-button":
+                        webbrowser.open("https://docs.google.com/forms/d/e/1FAIpQLSchHIjMhzE13S0FutPxqLsUEoATb8NTOlnoPVz7oxOZajTKPA/viewform?usp=sf_link")
+                    elif event.ui_object_id == "#exit-button":
+                        done = True
+
+                self.ui_manager.process_events(event)
+
+            time_delta = math.ceil(time.time()) - time_delta
+            self.redraw_elements(self.managers, time_delta)
