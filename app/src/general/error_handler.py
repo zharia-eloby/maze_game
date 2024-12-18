519026beed4429417eff7f4b41a429e2cf1789f0
zharia-eloby <55672825+zharia-eloby@users.noreply.github.com>
2024-12-17 20:05:11 -0600

    Issue 1 refactoring maze class (#8)
    * refactored functions
    
    - moved get_opposite_direction to helpers file
    - moved get_direction_from_cell_to_neighbor to cell class + renamed
    - removed __str__ functions
    
    * fixed SyntaxWarning

--- app/src/general/error_handler.py
+++ app/src/general/error_handler.py
@@ -6,7 +6,7 @@ class ErrorHandler:
 
         # set error text for error screen
         if (type(error) == FileNotFoundError):
-            filename = error.__str__().rsplit('\\')[-1].rstrip("\'\.")
+            filename = error.__str__().rsplit('\\')[-1].rstrip("\'.")
             error_screen.set_error_text('Missing file: {filename}'.format(filename=filename))
         else:
             error_screen.set_error_text(error.__class__.__name__)
