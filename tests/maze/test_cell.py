9f0543a6117834ac7ca8670b42dc011f63928f7e
zharia-eloby <zeloby22@gmail.com>
2025-01-18 21:53:44 -0600

    updated dir name for maze tests + updated test_cell.py

---
+++ tests/maze/test_cell.py
@@ -0,0 +1,30 @@
+from app.src.general.maze import Cell
+
+def test_get_blocked_walls():
+    cell = Cell(0, 0)
+
+    cell.walls = { "left": True, "right": False, "up": True, "down": False }
+    assert cell.get_blocked_walls() == ["left", "up"]
+
+    cell.walls = { "left": False, "right": False, "up": False, "down": False }
+    assert cell.get_blocked_walls() == []
+
+def test_get_open_walls():
+    cell = Cell(0, 0)
+
+    cell.walls = { "left": True, "right": False, "up": True, "down": False }
+    assert cell.get_open_walls() == ["right", "down"]
+
+    cell.walls = { "left": True, "right": True, "up": True, "down": True }
+    assert cell.get_open_walls() == []
+
+def test_get_direction_to_neighbor():
+    cell = Cell(1, 1)
+    neighbor = Cell(1, 2)
+    assert cell.get_direction_to_neighbor(neighbor) == "right"
+    assert neighbor.get_direction_to_neighbor(cell) == "left"
+
+    cell = Cell(2, 1)
+    neighbor = Cell(1, 1)
+    assert cell.get_direction_to_neighbor(neighbor) == "up"
+    assert neighbor.get_direction_to_neighbor(cell) == "down"
